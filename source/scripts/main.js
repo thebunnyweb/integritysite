d62050cdb469f91605eecc031b24b7a81bb171c0
Harsh Shah <harsh.shah@sapientrazorfish.com>
2017-06-06 19:11:57 +0530

    Homepage Desktop

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,10 +1,6 @@
 window.jQuery = window.$ = require('jquery');
 window.Bootstrap = require('bootstrap');
-var slick = require('slick-carousel');
-var videoBg = require('jquery-background-video');
+const slick = require('slick-carousel');
 
-
-const spotLight = require('../components/spotlight/spotlight.js');
-spotLight({
-	el : 'section#spotlight'
-});
+const spotlight = require('../components/spotlight/spotlight.js');
+spotlight();
